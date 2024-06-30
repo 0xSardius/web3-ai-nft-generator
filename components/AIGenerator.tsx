8dc7011de96c78c6f6c8c110a2a30877b134470e
Sardius <0xsardius@gmail.com>
2024-06-30 12:05:26 -0400

    initial minting component finished

--- components/AIGenerator.tsx
+++ components/AIGenerator.tsx
@@ -1,10 +1,66 @@
-import { ConnectButton } from "thirdweb/react";
+"use client";
+
+import { ConnectButton, MediaRenderer } from "thirdweb/react";
 import { client } from "@/app/client";
+import { useState } from "react";
 
 export const AIGenerator = () => {
+  const [imagePrompt, setImagePrompt] = useState("");
+  const [generatedImage, setGeneratedImage] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [isMinting, setIsMinting] = useState(false);
+
   return (
     <div className="flex flex-col items-center justify-center m-5">
       <ConnectButton client={client} />
+      <div className="m-5">
+        {generatedImage ? (
+          <MediaRenderer
+            client={client}
+            src={generatedImage}
+            className="w-[300px] h-[300px] rounded-lg"
+          />
+        ) : (
+          <div className="w-[300px] h-[300px] border border-dashed border-[#777] rounded-[10px] flex justify-center items-center">
+            <p className="color-[#777]">
+              {isGenerating
+                ? "Generating image..."
+                : "Enter a prompt to generate an image"}
+            </p>
+          </div>
+        )}
+      </div>
+      <form>
+        {!generatedImage || isMinting ? (
+          <div className="flex flex-col items-center">
+            <input
+              type="text"
+              placeholder="Enter a question..."
+              value={imagePrompt}
+              onChange={(e) => setImagePrompt(e.target.value)}
+              className="w-[300px] h-[40px] py-0 px-2.5 rounded-[5px] border border-solid border-[#777] mb-2.5"
+            />
+            <button
+              type="submit"
+              disabled={isGenerating || isMinting || !imagePrompt}
+              className="w-[300px] h=[40px] bg-[#333] text-[#fff] rounded-[5px] border-none cursor-pointer"
+            >
+              {isGenerating
+                ? "Generating..."
+                : isMinting
+                ? "Minting..."
+                : "Generate Image"}
+            </button>
+          </div>
+        ) : (
+          <button
+            onClick={() => setGeneratedImage("")}
+            className="w-[300px] h=[40px] bg-[#333] text-[#fff] rounded-[5px] border-none cursor-pointer"
+          >
+            Generate another NFT
+          </button>
+        )}
+      </form>
     </div>
   );
 };
